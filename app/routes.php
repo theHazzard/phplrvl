56433e5a5480575b9b7576f630a36dc8246fb2a8
Enzo Luis Strongoli <thehazzard@gmail.com>
2013-09-02 16:31:21 -0300

    saludo a rearte

--- app/routes.php
+++ app/routes.php
@@ -16,7 +16,7 @@ Route::get('/', function()
 	return View::make('hello');
 });
 
-Route::get('users', function()
+Route::get('joaquinRearte', function()
 {
-	return 'te amo majo';
+	return 'Alo Alo';
 });
