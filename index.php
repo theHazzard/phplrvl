f939088b541613071c710f7f0da406ff49fcffe8
Enzo Luis Strongoli <thehazzard@gmail.com>
2013-09-02 15:24:25 -0300

    initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<link rel="icon" type="image/ico" href="favicon.ico"/>
+<head>
+<title>Welcome to Wnmp!</title>
+</head>
+<body>
+<center><h2>Welcome to Wnmp 2.0.2.6!</h2></center>
+<center><h2>by <a href="https://www.x64Architecture.com/">Kurt Cancemi</a></h2></center>
+<center>Click <a href="https://localhost/phpmyadmin/">here</a> to go to phpMyAdmin</center>
+<br></br>
+<?php 
+phpinfo(); 
+?>
+</body>
+</html>
